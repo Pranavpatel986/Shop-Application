f8ebf283e0a2d2d8fec4f2a7bf9048d7b8f99b03
pranav <pranavpatel986@gmail.com>
2022-03-26 23:39:06 +0530

    Add files via upload

---
+++ Simplilearn-phase-end-project/src/main/java/com/simplilearn/service/OrderDetailService.java
@@ -0,0 +1,21 @@
+package com.simplilearn.service;
+
+import com.simplilearn.model.OrderDetail;
+import com.simplilearn.repo.OrderDetailRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class OrderDetailService {
+    @Autowired
+    private OrderDetailRepo orderDetailRepo;
+    Optional<OrderDetail> findById(Long id) {
+        return orderDetailRepo.findById(id);
+    }
+
+    public void save(OrderDetail orderDetail) {
+        orderDetailRepo.save(orderDetail);
+    }
+}
